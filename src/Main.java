abe381f5c8c8434bbb08e30e929667ba23fe041b
skazak77 <serzh-kazakov-1977@mail.ru>
2022-09-26 15:26:09 +0500

    Сделано ДЗ

---
+++ src/Main.java
@@ -0,0 +1,63 @@
+public class Main {
+    public static void main(String[] args) {
+  var box=5;
+        System.out.println(box);
+  box=box+2;
+        System.out.println(box);
+        box=box-3;
+        System.out.println(box);
+        var dog=8;
+        System.out.println(dog);
+        var cat=3.6;
+        System.out.println(cat);
+        var paper=763789;
+        System.out.println(paper);
+        dog=dog+4;
+        System.out.println(dog);
+        cat=cat+4;
+        System.out.println(cat);
+        paper=paper+4;
+        System.out.println(paper);
+        double dog1;
+        dog1 = dog - 3.5;
+        System.out.println(dog);
+        cat=cat-1.6;
+        System.out.println(cat);
+        paper=paper-7639;
+        System.out.println(paper);
+        var  friend=19;
+        System.out.println( friend);
+        friend= friend+2;
+        System.out.println( friend);
+        friend= friend/7;
+      System.out.println( friend);
+        var frog=3.5;
+        frog=frog*10;
+        System.out.println(frog);
+        frog=frog/3.5;
+        System.out.println(frog);
+        frog=frog+4;
+        System.out.println(frog);
+        var weightBig=82.7;
+        var weightSmall=78.2;
+        var commonB=weightBig+weightSmall;
+        System.out.println("Общий вес бойцов"+commonB+"кг");
+        var sport1=82.7;
+        var soort2=78.2;
+        var difSp=sport1-soort2;
+        System.out.println("Разница веса спортсменов"+difSp+"кг");
+        var ostatDel=sport1%soort2;
+        System.out.println("Остаток от деления"+ostatDel+"кг");
+        var hoursEveryone=640;
+        var sotrOne=8;
+        var sotrAll=hoursEveryone/sotrOne;
+        System.out.println("Всего работников в кампании"+sotrAll+"человек");
+      var  sotrDoubl=94;
+      var sotrPlus=sotrAll+sotrDoubl;
+System.out.println(sotrPlus);
+var hoursDell=sotrPlus*sotrOne;
+System.out.println("Если в компании работает" + sotrPlus + "человек" + "то всего"+ hoursDell +"часов работы может быть поделено между сотрудниками");
+
+
+    }
+}
