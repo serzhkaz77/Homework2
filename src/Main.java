5436087976ded09a39c9b971a854c5f0cb0f8e37
skazak77 <serzh-kazakov-1977@mail.ru>
2022-09-26 15:26:09 +0500

    Переменные

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+  var box=5;
+        System.out.println(box);
+  box=box+2;
+        System.out.println(box);
+        box=box-3;
+        System.out.println(box);
+    }
+}
